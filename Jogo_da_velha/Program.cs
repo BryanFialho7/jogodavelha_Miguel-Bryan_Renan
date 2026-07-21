40c9808dfd0ddab1d683f6e95608191f4eb56934
Bryan <bryan.f.souza10@aluno.senai.br>
2026-07-21 15:07:01 -0300

    Adicionando o menu do jogo

---
+++ Jogo_da_velha/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    bool Menu = true;
+
+    static void Main()
+    {
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.WriteLine("Jogo da Velha");
+        Console.WriteLine("1 - Iniciar partida");
+        Console.WriteLine("2 - Créditos");
+        Console.WriteLine("3 - Sair");
+        Console.Readline();
+
+        
+    }
+
+}
