3f3c16ecf30bbb3790fba9488eda3e35a0f1d77f
renandevvz <renangomesdefreitas05@gmail.com>
2026-07-21 16:26:10 -0300

    modificando o jogo

--- Jogo_da_velha/Program.cs
+++ Jogo_da_velha/Program.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 class Program
 {
-    bool Menu = true;
-
     static void Main()
     {
-        Menu();
+         Menu();
     }
-
     static void Menu()
+        {
+            Console.WriteLine("Digite o seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Seja Bem Vindo " + nome + "!");
+            Jogo();
+        }
+    static void Jogo()
     {
-        Console.WriteLine("Jogo da Velha");
-        Console.WriteLine("1 - Iniciar partida");
-        Console.WriteLine("2 - Créditos");
-        Console.WriteLine("3 - Sair");
-        Console.Readline();
 
-        
+    Console.WriteLine("O jogo começou!");
+    Console.WriteLine("  1  |  2  |  3  ");
+    Console.WriteLine("----------------");  
+    Console.WriteLine("  4  |  5  |  6 ");
+    Console.WriteLine("----------------");  
+    Console.WriteLine("  7  |  8  |  9  ");
+
+    Console.WriteLine("Escolha uma posição:");
+    string posicao = Console.ReadLine();
+    if(posicao == " ")
+        {
+            Console.WriteLine("X");
+        }
+
+
     }
-
 }
--- Jogo_da_velha/obj/Debug/net10.0/Jogo_da_velha.AssemblyInfo.cs
+++ Jogo_da_velha/obj/Debug/net10.0/Jogo_da_velha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jogo_da_velha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+413880e6052d2bd1c753567b3895f28540e0e6cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a564150aee3c24897bdcbd001e1a8a9716a05f7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jogo_da_velha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jogo_da_velha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
